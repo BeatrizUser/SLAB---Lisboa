7fd05ce5c5210f416742c922409af363d7f24714
BeatrizUser <beatriz.machadosilva@gmail.com>
2022-05-11 17:39:37 -0300

    Bug ok

--- slab_lisboa/cadastro/urls.py
+++ slab_lisboa/cadastro/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 app_name = "Cadastro"
 urlpatterns = [
-    path("", views.CadListView.as_view(), name="list"),
+    path("lista/", views.CadListView.as_view(), name="list"),
     path("<slug:slug>/", views.CadDetailView.as_view(), name="detail")
 ]
