e0b4ad4df7f254ad22f7e88f7a0c5c26b7c9bc61
BeatrizUser <beatriz.machadosilva@gmail.com>
2022-05-13 01:49:22 -0300

    melhorias na interface garfica do site

--- slab_lisboa/cadastro/admin.py
+++ slab_lisboa/cadastro/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Cadastro
+from .models import Cadastro, calc_idade
 
 @admin.register(Cadastro)
 class CadastroAdmin(admin.ModelAdmin):
 	list_display = ("nome_completo_aluno", "created",)
 	prepopulated_fields = {"slug": ("nome_completo_aluno",)}
+	#prepopulated_fields = {"idade": (calc_idade(Cadastro.data_de_Nascimento_do_Aluno),)}
+	
--- slab_lisboa/cadastro/models.py
+++ slab_lisboa/cadastro/models.py
@@ -1,3 +1,4 @@
+from datetime import date 
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
@@ -6,7 +7,17 @@ from django.utils import timezone
 def upload_image_aluno(instance, filename):
     return f"{instance.nome_completo_aluno}-{filename}"
 
+def calc_idade(birthDate):
+    today = date.today() 
+    age = today.year - birthDate.year - ((today.month, today.day) < (birthDate.month, birthDate.day)) 
+    return (age(date(1997, 2, 3)), "anos")
+
+
 class Cadastro(models.Model):
+    ALERGIA_CHOICES = (
+    ('Nao possui Alergia', 'Nao'),
+    ('Possui Alergia', 'Sim'),
+    )
 
     author =  models.ForeignKey(User, on_delete=models.CASCADE)
     email = models.EmailField(max_length = 75)
@@ -14,13 +25,16 @@ class Cadastro(models.Model):
     data_de_Nascimento_do_Aluno = models.DateField('Data de Nascimento do Aluno', default=timezone.now)
     morada = models.CharField(max_length = 200)
     nome_completo_responsavel =  models.CharField('Nome do Responsavel', blank=False, max_length=255)
-    #contato_do_responsavel= models.CharField(max_length = 12)
+    idade = models.CharField('Idade', max_length=2)
+    contato_do_responsavel= models.CharField(max_length = 12)
     documento_de_id = models.CharField(max_length = 20)
     email_do_responsavel= models.EmailField('Email', max_length = 75)
+    comentarios = models.CharField(max_length=255, default=timezone.now)
     foto = models.ImageField(upload_to=upload_image_aluno, blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     slug = models.SlugField('Campo preenchido automaticamente', max_length=255, unique=True)
+    alergia = models.CharField('Possui Alergias?', max_length=100, choices=ALERGIA_CHOICES)
 
     #Este código altera a ordem da lista de post pela ordem de postagem
     class Meta:
